e992e56656b4eea9dc543ef260136caf148b1161
Víctor Mayoral Vilches <victor@erlerobot.com>
2017-12-18 18:51:42 +0100

    Add changes to include tensorboard

--- baselines/deepq/simple.py
+++ baselines/deepq/simple.py
@@ -216,6 +216,9 @@ def learn(env,
     obs = env.reset()
     reset = True
     with tempfile.TemporaryDirectory() as td:
+        # Add a filewriter to get tensorboard details
+        outdir = '/tmp/experiments/DQN'
+        summary_writer = tf.summary.FileWriter(outdir, graph=tf.get_default_graph())
         model_saved = False
         model_file = os.path.join(td, "model")
         for t in range(max_timesteps):
@@ -247,6 +250,11 @@ def learn(env,
 
             episode_rewards[-1] += rew
             if done:
+                # Log the episode reward
+                summary = tf.Summary(value=[tf.Summary.Value(tag="Episode reward", simple_value = episode_rewards[-1])])
+                summary_writer.add_summary(summary, t)
+                summary_writer.flush()
+
                 obs = env.reset()
                 episode_rewards.append(0.0)
                 reset = True
