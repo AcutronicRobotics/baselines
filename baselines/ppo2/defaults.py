5edc1626fd6a4e8c5dd34e1fc90a9bd07b36cd72
rkojcev <rkojcev@gmail.com>
2018-09-20 10:43:35 +0200

    adding modcobot

--- baselines/ppo2/defaults.py
+++ baselines/ppo2/defaults.py
@@ -20,3 +20,17 @@ def atari():
         lr=lambda f : f * 2.5e-4,
         cliprange=lambda f : f * 0.1,
     )
+
+def modcobot():
+    return dict(
+        nsteps=2048,
+        nminibatches=32,
+        lam=0.95,
+        gamma=0.99,
+        noptepochs=10,
+        log_interval=1,
+        ent_coef=0.0,
+        lr=lambda f: 3e-4 * f,
+        cliprange=0.2,
+        value_network='copy'
+    )
