1d99308ee71e4d67a2d98eaf10a2acc082c40185
yueerro <yue.trbj@gmail.com>
2019-01-31 16:57:21 +0100

    replace mara with mara_mlp in ppo2 defaults

--- baselines/ppo2/defaults.py
+++ baselines/ppo2/defaults.py
@@ -23,26 +23,28 @@ def atari():
 def retro():
    return atari()
 
-def mara():
+def mara_mlp():
     return dict(
         num_layers = 4,
         num_hidden = 128,
-        nsteps=2048,
-        nminibatches=8, #batchsize = nevn * nsteps // nminibatches
-        lam=0.95,
-        gamma=0.99,
-        noptepochs=10,
-        log_interval=1,
-        ent_coef=0.0,
-        lr=lambda f: 3e-4 * f,
-        cliprange=0.2,
-        vf_coef=0.5,
-        seed=0,
-        max_grad_norm=0.5,
-        value_network='copy',
-        network='mlp',
-        total_timesteps=1e8,
-        save_interval=10
+        layer_norm = False,
+        nsteps = 2048,
+        nminibatches = 32,
+        lam = 0.95,
+        gamma = 0.99,
+        noptepochs = 10,
+        log_interval = 1,
+        ent_coef = 0.0,
+        lr = lambda f: 3e-4 * f,
+        cliprange = 0.2,
+        vf_coef = 0.5,
+        max_grad_norm = 0.5,
+        seed = 0,
+        value_network = 'copy',
+        network = 'mlp',
+        total_timesteps = 1e8,
+        save_interval = 10,
+        num_envs = 1
     )
 
 def mara_lstm():
@@ -53,24 +55,24 @@ def mara_lstm():
         # assert batchsize == nbatch_train >= nsteps
         # nan < 1024
         # gpu > 512
-        nsteps=1024,
+        nsteps = 1024,
         #otherwise, last minibatch gets noisy gradient,
         # careful this by default is 1, please change it in your script
-        nminibatches=2, #batchsize = nevn * nsteps // nminibatches
-        lam=0.95,
-        gamma=0.99,
-        noptepochs=10,
-        log_interval=1,
-        ent_coef=0.0,
-        lr=lambda f: 3e-4 * f,
-        cliprange=0.2,
-        vf_coef=0.5,
-        seed=0,
-        max_grad_norm=0.5,
-        value_network='shared',
-        network='lstm',
-        nlstm=256,
-        layer_norm=False,
-        total_timesteps=1e8,
-        save_interval=10
+        nminibatches = 2, #batchsize = nevn * nsteps // nminibatches
+        lam = 0.95,
+        gamma = 0.99,
+        noptepochs = 10,
+        log_interval = 1,
+        ent_coef = 0.0,
+        lr = lambda f: 3e-4 * f,
+        cliprange = 0.2,
+        vf_coef = 0.5,
+        seed = 0,
+        max_grad_norm = 0.5,
+        value_network = 'shared',
+        network = 'lstm',
+        nlstm = 256,
+        layer_norm = False,
+        total_timesteps = 1e8,
+        save_interval = 10
     )
