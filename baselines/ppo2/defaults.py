408407b4b762866462abc8ec77229fc8eb91e474
yueerro <yue.trbj@gmail.com>
2019-02-01 11:01:21 +0100

    minor change

--- baselines/ppo2/defaults.py
+++ baselines/ppo2/defaults.py
@@ -49,6 +49,8 @@ def mara_mlp():
 
 def mara_lstm():
     return dict(
+        nlstm = 256,
+        layer_norm = False,
         # nbatch = nenvs * nsteps
         # nbatch_train = nbatch // nminibatches
         # assert nbatch % nminibatches == 0
@@ -67,14 +69,12 @@ def mara_lstm():
         lr = lambda f: 3e-4 * f,
         cliprange = 0.2,
         vf_coef = 0.5,
-        seed = 0,
         max_grad_norm = 0.5,
+        seed = 0,
         value_network = 'shared',
         network = 'lstm',
-        nlstm = 256,
-        layer_norm = False,
         total_timesteps = 1e8,
         save_interval = 10,
         env_name = 'MARA-v0',
-        num_envs = 1
+        num_envs = 2
     )
