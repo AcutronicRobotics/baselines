5674b83a8af69583c1ab182869125ae09e589b19
yueerro <yue.trbj@gmail.com>
2019-02-19 13:18:56 +0100

    fix import and add mara hyperparameters

--- baselines/trpo_mpi/defaults.py
+++ baselines/trpo_mpi/defaults.py
@@ -1,5 +1,4 @@
-from rl_common.models import mlp, cnn_small
-
+from baselines.common.models import mlp, cnn_small
 
 def atari():
     return dict(
@@ -27,4 +26,33 @@ def mujoco():
         vf_iters=5,
         vf_stepsize=1e-3,
         normalize_observations=True,
+    )
+
+def mara():
+    return dict(
+        num_layers = 2,
+        num_hidden = 64,
+        layer_norm = False,
+        timesteps_per_batch=1024,
+        total_timesteps = 1e8,
+        max_kl=0.01,
+        cg_iters=10,
+        gamma=0.99,
+        lam=0.98,
+        seed = 0,
+        ent_coef=0.0,
+        cg_damping=0.1,
+        vf_stepsize=1e-3,
+        vf_iters=5,
+        max_episodes=0,
+        max_iters=0,
+        normalize_observations=True,
+        save_interval = 10,
+        # env_name = 'MARA-v0',
+        # env_name = 'MARAOrient-v0',
+        env_name = 'MARACollision-v0',
+        # env_name = 'MARACollisionOrient-v0',
+        transfer_path = None,
+        # transfer_path = '/tmp/ros_rl2/MARACollision-v0/trpo_mpi/2019-02-19_13h02min/checkpoints/best',
+        trained_path = '/tmp/ros_rl2/MARACollision-v0/trpo_mpi/2019-02-19_13h11min/checkpoints/best'
     )
