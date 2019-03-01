37c8101ffc009d5c4188fc4a2d5e2b6fe139290e
EliasBarba <elias.barba@gmail.com>
2019-03-01 11:23:50 +0100

    update defaults

--- baselines/ppo2/defaults.py
+++ baselines/ppo2/defaults.py
@@ -25,32 +25,38 @@ def retro():
 
 def mara_mlp():
     return dict(
-        num_layers = 3,
+        num_layers = 2,
         num_hidden = 64,
         layer_norm = False,
-        nsteps = 1024,
-        nminibatches = 4, #batchsize = nevn * nsteps // nminibatches
+        nsteps = 2048,
+        nminibatches = 32, #batchsize = nevn * nsteps // nminibatches
         lam = 0.95,
         gamma = 0.99,
         noptepochs = 10,
         log_interval = 1,
         ent_coef = 0.0,
-        lr = lambda f: 6e-4 * f,
+        lr = lambda f: 3e-4 * f,
         cliprange = 0.2,
         vf_coef = 0.5,
         max_grad_norm = 0.5,
         seed = 0,
         value_network = 'copy',
         network = 'mlp',
-        total_timesteps = 1e8,
+        total_timesteps = 1e6,
         save_interval = 10,
+        _alpha = 6,
+        _beta = 3,
+        _gamma = 3,
+        _delta = 3,
+        _eta = 0.03,
+        done = 0.02,
         # env_name = 'MARA-v0',
         # env_name = 'MARAOrient-v0',
-        env_name = 'MARACollision-v0',
-        # env_name = 'MARACollisionOrient-v0',
+        # env_name = 'MARACollision-v0',
+        env_name = 'MARACollisionOrient-v0',
         transfer_path = None,
         # transfer_path = '/tmp/ros_rl2/MARACollision-v0/ppo2_mlp/2019-02-19_12h47min/checkpoints/best',
-        trained_path = '/media/yue/hard_disk/ros_rl2/MARACollision-v0/ppo2_mlp/2019-02-22_17h41min/checkpoints/best'
+        trained_path = '/home/elias/Experiments/MARAOrient-v0/ppo2_mlp/2019-02-28_14h24min/checkpoints/00570'
     )
 
 def mara_lstm():
