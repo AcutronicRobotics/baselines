dc39603fa30ad63cde86ab6264c4c0db3c087bbf
pzhokhov <peterz@openai.com>
2019-04-26 16:14:21 -0700

    change rms 2 tfrms switch in vec_normalize to be more explicit (#886)
    * change rms 2 tfrms switch in vec_normalize to be more explicit
    
    * modify the vec_normalize / use_tf logic a little bit
    
    * typo
    
    * use_tf = False by default

--- baselines/run.py
+++ baselines/run.py
@@ -112,7 +112,7 @@ def build_env(args, render=False):
         env = make_vec_env(env_id, env_type, args.num_env or 1, seed, reward_scale=args.reward_scale, flatten_dict_observations=flatten_dict_observations)
 
         if env_type == 'mujoco':
-            env = VecNormalize(env)
+            env = VecNormalize(env, use_tf=True)
 
     return env
 
